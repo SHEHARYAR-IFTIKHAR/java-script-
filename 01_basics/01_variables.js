8ae25df510e173da79c97dcf50bb9dd253443f8e
SHEHARYAR-IFTIKHAR <149076095+SHEHARYAR-IFTIKHAR@users.noreply.github.com>
2024-01-27 15:52:33 +0000

    update files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountEmail = "1234@gmail.com";
+let accountPassword = "12345";
+var accountCity = "Karachi";
+
+// accountId = 144554;const means constant it will not allow to change the value of constant
+accontEmail = "Sheharyar123@gmail.com";//let variable we can change the value of let any time but we cannot create another variable with same name.
+
+accountPassword = "1212";// var variable we can change the value of var variable and we can create another variable with same name.
+
+accountCity = "Lahore";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
